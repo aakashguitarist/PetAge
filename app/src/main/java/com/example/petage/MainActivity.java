e586052a064f9820f9f0de6b0c9759502f4cf614
aakashguitarist <kumaraaksh323@gmail.com>
2019-09-03 23:36:45 +0530

    Second GitHubAndroid Studio Commit

--- app/src/main/java/com/example/petage/MainActivity.java
+++ app/src/main/java/com/example/petage/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+    int i;
     }
 
     public void agefun(View v)
